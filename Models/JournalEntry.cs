72232a439d0cb2d813298183d8dd4105180008e4
Jid Kimachia <kimachiajidraf@gmail.com>
2025-11-05 18:16:41 +0300

    fix(model): add DataAnnotations to JournalEntry for key and identity generation

--- Models/JournalEntry.cs
+++ Models/JournalEntry.cs
@@ -1,10 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JournalApi.Models
 {
     public class JournalEntry
     {
-        public int Id {get;set;}
-        public string? Title {get;set;}
-        public string? Content {get;set;}
-        public DateTime CreatedAt {get;set;} = DateTime.Now;
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
--- obj/Debug/net9.0/JournalApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/JournalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217d100f3e79638461ffee6fa6971139deca5d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f211a41c6c8f119d59f3f594e30788b73f6f8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
